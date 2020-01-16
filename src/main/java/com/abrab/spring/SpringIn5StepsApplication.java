e8721ee86d5f46c9c04eb5a6b2440466df6be503
abdo <abrab93@gmail.com>
2020-01-16 14:41:27 +0100

    example for closing applicationContext after using it

--- src/main/java/com/abrab/spring/SpringIn5StepsApplication.java
+++ src/main/java/com/abrab/spring/SpringIn5StepsApplication.java
@@ -12,14 +12,17 @@ public class SpringIn5StepsApplication {
 
     public static void main(String[] args) {
 
-        ConfigurableApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringIn5StepsApplication.class);
-        PrestationImpl assurance = applicationContext.getBean(PrestationImpl.class);
-        PrestationImpl assurance2 = applicationContext.getBean(PrestationImpl.class);
-        System.out.println(assurance);
-        System.out.println(assurance2);
-        double result = assurance.assuranceMaladieObligatoire(100);
-        result = assurance2.assuranceMaladieObligatoire(100);
-        System.out.println(result);
+        try (ConfigurableApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringIn5StepsApplication.class)) {
+            PrestationImpl assurance = applicationContext.getBean(PrestationImpl.class);
+            PrestationImpl assurance2 = applicationContext.getBean(PrestationImpl.class);
+            System.out.println(assurance);
+            System.out.println(assurance2);
+            double result = assurance.assuranceMaladieObligatoire(100);
+            result = assurance2.assuranceMaladieObligatoire(100);
+            System.out.println(result);
+
+            // or use applicationContext.close(); rather than resource try
+        }
     }
 
 }
