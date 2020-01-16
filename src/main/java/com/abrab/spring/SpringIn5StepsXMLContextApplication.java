3acf2e9c66d103b505c019898b1fac68f9e11f0a
abdo <abrab93@gmail.com>
2020-01-16 16:04:48 +0100

    mixing annotation with xml config via context-component-scan tag, to enable spring to look for class annotated with @Component

--- src/main/java/com/abrab/spring/SpringIn5StepsXMLContextApplication.java
+++ src/main/java/com/abrab/spring/SpringIn5StepsXMLContextApplication.java
@@ -1,17 +1,11 @@
 package com.abrab.spring;
 
-import com.abrab.spring.basics.PrestationImpl;
 import com.abrab.spring.xml.XmlPersonDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class SpringIn5StepsXMLContextApplication {
 
     private static Logger logger = LoggerFactory.getLogger(SpringIn5StepsXMLContextApplication.class);
@@ -19,6 +13,9 @@ public class SpringIn5StepsXMLContextApplication {
     public static void main(String[] args) {
 
         try (ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+
+            logger.info("beans loaded => {}",
+                    (Object) applicationContext.getBeanDefinitionNames());
 
             XmlPersonDao xmlPersonDao = applicationContext.getBean(XmlPersonDao.class);
 
